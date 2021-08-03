e30836e4cb34df772b8a85979b0ea14bb29b7498
dao321 <daomengwang@163.com>
2021-08-03 19:26:31 +0800

    num2

---
+++ main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>//printf函数 输入和输出
+int main(void)//定义函数 返回值为整型 无参数 f()=int 返回值为int
+{
+    int num;//定义一个int num为地址名
+    num = 1;//给num赋值  为1
+
+    printf("i am a simple ");
+    printf("computer.\n");
+    printf("my favorite number is %d because it is first.\n",num);
+
+
+    return 0;
+}
